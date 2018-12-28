72cc50a3c391f38f70736c8b943f163abbe5ef52
leopard-gecko <kaz_bashi@yahoo.co.jp>
2018-12-28 17:35:37 +0900

    Add files via upload

---
+++ rules.mk
@@ -0,0 +1,9 @@
+BACKLIGHT_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE  = no
+
+CONSOLE_ENABLE = no         # Console for debug(+400)
+
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
